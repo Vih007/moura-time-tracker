0b2ade9f2373db48dbdd1f25d6d9a374048b995f
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2026-01-16 17:23:15 -0300

    chore(backend): adiciona estrutura base inicial

---
+++ backend/src/main/java/br/com/moura/time_tracker/MouraTimeTrackerApplication.java
@@ -0,0 +1,13 @@
+package br.com.moura.time_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MouraTimeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MouraTimeTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/moura/time_tracker/MouraTimeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moura.time_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MouraTimeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
