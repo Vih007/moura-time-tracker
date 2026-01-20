5ea67d52d7cea7d2e4d3c1ccfdbfb2113f3e0c18
Vitoria <melovitoria763@gmail.com>
2026-01-19 22:55:01 -0300

    colocar *

--- backend/src/main/java/br/com/moura/time_tracker/config/CorsConfig.java
+++ backend/src/main/java/br/com/moura/time_tracker/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://moura-time-tracker.vercel.app/", "http://localhost:3000", "http://127.0.0.1:5173")
+                .allowedOrigins("http://localhost:5173", "https://moura-time-tracker.vercel.app", "*", "http://localhost:3000", "http://127.0.0.1:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
